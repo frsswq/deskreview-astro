07ef5100654217c0ef8b2ebe2b6013b2a0765afe
frsswq <farissaifuddin.work@gmail.com>
2025-07-30 23:48:08 +0700

    fix: layout shift navbar

--- src/components/navbar-react.tsx
+++ src/components/navbar-react.tsx
@@ -28,27 +28,27 @@ export default function Navbar() {
       }}
       animate={hidden ? "hidden" : "visible"}
       transition={{ duration: 0.3, ease: "easeInOut" }}
-      className="sticky top-0 z-100 flex max-h-[80px] justify-center border border-transparent border-b-slate-300 bg-white"
+      className="sticky top-0 z-100 flex max-h-[80px] min-h-[72px] justify-center border border-transparent border-b-slate-300 bg-white"
     >
       <div className="flex h-full w-full max-w-[1440px] items-center justify-between px-5 py-5 md:px-15">
         <a href="/" className="flex h-full hover:cursor-pointer">
           {isMobile ? (
             <LogoIcon
-              width="40"
-              height="40"
-              className="-mt-1 h-full object-contain"
+              width="36"
+              height="36"
+              className="-mt-1 block h-full object-contain md:hidden"
             />
           ) : (
             <LogoFull
               width="201"
               height="36"
-              className="-mt-1 h-full object-contain"
+              className="-mt-1 hidden h-full object-contain md:block"
             />
           )}
         </a>
 
         {!isMobile && (
-          <div className="flex items-center gap-x-10 font-[350] font-sans text-lg leading-none tracking-[-0.01em]">
+          <div className="hidden items-center gap-x-10 font-[350] font-sans text-lg leading-none tracking-[-0.01em] md:flex">
             <a
               href="/"
               className="hover:cursor-pointer hover:text-desk-blue/90 hover:underline"
